e6c13460127619316d22617097758a3428675f57
lakhankr0399 <lakhankr031999@gmail.com>
2026-07-13 14:20:15 +0530

    update

--- src/main/java/com/learning/Main.java
+++ src/main/java/com/learning/Main.java
@@ -8,19 +8,12 @@ import com.learning.model.Employee;
 public class Main {
 
 	public static void main(String[] args) {
-		ApplicationContext ioc=new ClassPathXmlApplicationContext("application.context.xml");
+		ApplicationContext ioc=new ClassPathXmlApplicationContext("inner-bean.xml");
 		
 		System.out.println("<<<<<<<----->>>>>>>>");
 		Employee bean=ioc.getBean("emp1", Employee.class);
 		System.out.println(bean);
-		
-//		Employee bean2=ioc.getBean("emp2", Employee.class);
-//		System.out.println(bean2);
-		
-		
-		
-		
-		
+			
 
 	}
 
